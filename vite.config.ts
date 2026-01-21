29a815c06049b2bdb5c51c212ded82365f226f23
Corny Ngo <nlhthinh95@gmail.com>
2026-01-21 14:30:05 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Đảm bảo các file .js, .css tìm thấy nhau trên GitHub Pages
+  base: '', // Sử dụng base rỗng để các đường dẫn tự động thích ứng với GitHub Pages
   build: {
     outDir: 'dist',
-  },
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 });
